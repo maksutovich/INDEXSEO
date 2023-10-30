751dbacae1dc6936b4ef1b3cc40956b7ec92bc12
maksutovich <149145409+maksutovich@users.noreply.github.com>
2023-10-30 10:45:25 +1000

    Delete bin/Загрузить Python.cmd

--- bin/Загрузить Python.cmd
+++
@@ -1,6 +0,0 @@
-python-3.9.0-amd64-webinstall /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-python pipi.py
-pip install google-api-python-client oauth2client
-pip install --upgrade oauth2client
-del pipi.py
-del python-3.12.0-amd64.exe
