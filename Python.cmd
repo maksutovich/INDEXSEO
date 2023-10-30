746047f186c065f2947eea46df52203b07a34882
maksutovich <149145409+maksutovich@users.noreply.github.com>
2023-10-30 11:09:44 +1000

    Add files via upload

---
+++ Python.cmd
@@ -0,0 +1,10 @@
+python-3.9.0-amd64-webinstall /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+python pipi.py
+pip install google-api-python-client oauth2client
+pip install --upgrade oauth2client
+del pipi.py
+del python-3.9.0-amd64-webinstall.exe
+del Python.cmd
+del LICENSE
+del README.md
+del .gitignore
